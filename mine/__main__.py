ab7bc0fbb5d6b584e68cc69f0ccbcc712313919d
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2025-02-22 12:21:41 +0530

    Update __main__.py

--- mine/__main__.py
+++ mine/__main__.py
@@ -34,7 +34,7 @@ user_points = {}  # Dictionary to store user points
 # Bot credentials
 API_ID = int(os.getenv("API_ID", "20222660"))
 API_HASH = os.getenv("API_HASH", "5788f1f4a93f2de28835a0cf1b0ebae4")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7560532835:AAE5yA7zLwHrkJQK0VYeGeCR-Db6Jhqzvpo")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6694970760:AAFv6Zm9Av8HrY7JOTohg0E6c53Ar036eDc")
 
 app = Client("word_guess_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
