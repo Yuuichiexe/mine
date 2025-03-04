474699b89a1734f65b20b746f6c6aea5e240d91b
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2025-03-04 21:52:42 +0530

    Update __init__.py

--- mine/__init__.py
+++ mine/__init__.py
@@ -6,3 +6,6 @@ API_HASH = os.getenv("API_HASH", "5788f1f4a93f2de28835a0cf1b0ebae4")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "6694970760:AAFv6Zm9Av8HrY7JOTohg0E6c53Ar036eDc")
 
 app = Client("word_guess_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+
+
+app.run()
