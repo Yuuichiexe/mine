351f391a5d1e5feec73bde35275cf644e3e9cc52
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2025-03-04 22:33:10 +0530

    Update __main__.py

--- mine/__main__.py
+++ mine/__main__.py
@@ -1,6 +1,9 @@
 import os
 from pyrogram import Client
 from mine import app
+from mine.game import *
+from mine.challenge import *
+from database import *
 
 
 print("WORD MINE BOT HAS BEEN STARTED✅")
