a255fb92d4a26b1c85a06ed624ae611781d6b426
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2025-03-05 19:49:07 +0530

    Update challenge.py

--- mine/challenge.py
+++ mine/challenge.py
@@ -5,7 +5,7 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from database import get_user_points, update_user_points
 from mine import app
-from mine.game import challenge_data
+from mine.game import challenger_data
 # Store ongoing challenges
 
 
